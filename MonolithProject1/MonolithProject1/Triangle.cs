f6a1a8dc77f4addb76c472545d7eec4d9126d274
Carlos Ty <carlos.ty@relativity.com>
2020-01-27 15:34:29 -0600

    Implemenet Triangle Class

---
+++ MonolithProject1/MonolithProject1/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonolithProject1
+{
+	public class Triangle
+	{
+		private int  side1;
+		private int side2;
+		private int side3;
+
+		public Triangle(int s1, int s2, int s3)
+		{
+			side1 = s1;
+			side2 = s2;
+			side3 = s3;
+		}
+
+	}
+}
